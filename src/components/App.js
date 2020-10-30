7f5566c76e74ffdf0299ffcf791adc86e21c2908
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-10-30 17:22:40 +0530

    Initial Cahnges

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,38 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+    this.state = {
+      clock: new Date()
+    };
+  }
+
+  updateClock() {
+    this.setState = {
+      clock: new Date()
+    };
+    //console.log("Hi");
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.updateClock();
+    }, 1000);
+  }
+
+  componetWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.clock.toLocaleTimeString()}</h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
